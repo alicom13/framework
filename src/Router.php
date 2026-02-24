13e4b71d1ff7b5cc9835bbf9ddff0cd7995185be
Ali Musthofa <alfaonline13@gmail.com>
2026-02-25 06:16:45 +0700

    Update Router.php

--- src/Router.php
+++ src/Router.php
@@ -1,94 +1,134 @@
 <?php
+
 namespace Mas\Ali;
 
-class Router {
+class Router
+{
     private static $routes = [];
-    private static $notFound = null;
-    private static $basePath = '';
-    
-    public static function setBasePath($path) {
-        self::$basePath = rtrim($path, '/');
+    private static $filters = [];
+    private static $notFound;
+
+    public static function get($path, $handler, $filters = [])
+    {
+        self::$routes['GET'][] = [
+            'path' => $path,
+            'handler' => $handler,
+            'filters' => $filters
+        ];
     }
-    
-    public static function add($method, $path, $handler) {
-        self::$routes[strtoupper($method)][$path] = $handler;
+
+    public static function post($path, $handler, $filters = [])
+    {
+        self::$routes['POST'][] = [
+            'path' => $path,
+            'handler' => $handler,
+            'filters' => $filters
+        ];
     }
-    
-    public static function get($path, $handler) {
-        self::add('GET', $path, $handler);
+
+    public static function put($path, $handler, $filters = [])
+    {
+        self::$routes['PUT'][] = [
+            'path' => $path,
+            'handler' => $handler,
+            'filters' => $filters
+        ];
     }
-    
-    public static function post($path, $handler) {
-        self::add('POST', $path, $handler);
+
+    public static function delete($path, $handler, $filters = [])
+    {
+        self::$routes['DELETE'][] = [
+            'path' => $path,
+            'handler' => $handler,
+            'filters' => $filters
+        ];
     }
-    
-    public static function put($path, $handler) {
-        self::add('PUT', $path, $handler);
-    }
-    
-    public static function delete($path, $handler) {
-        self::add('DELETE', $path, $handler);
-    }
-    
-    public static function notFound($handler) {
+
+    public static function notFound($handler)
+    {
         self::$notFound = $handler;
     }
-    
-    public static function run() {
-        $method = $_SERVER['REQUEST_METHOD'];
+
+    public static function addFilter($filter)
+    {
+        self::$filters[] = $filter;
+    }
+
+    public static function run()
+    {
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        
-        // Remove base path
-        if (self::$basePath && strpos($uri, self::$basePath) === 0) {
-            $uri = substr($uri, strlen(self::$basePath));
-        }
-        
-        $uri = rtrim($uri, '/') ?: '/';
-        
-        // Cek route
+        $method = $_SERVER['REQUEST_METHOD'];
+        $routeMatched = false;
+
         if (isset(self::$routes[$method])) {
-            foreach (self::$routes[$method] as $path => $handler) {
-                $pattern = self::patternToRegex($path);
-                
+            foreach (self::$routes[$method] as $route) {
+                $pattern = self::patternToRegex($route['path']);
                 if (preg_match($pattern, $uri, $matches)) {
+                    $routeMatched = true;
                     array_shift($matches);
-                    return self::callHandler($handler, $matches);
+
+                    foreach (self::$filters as $filter) {
+                        if (class_exists($filter)) {
+                            $filter::before();
+                        }
+                    }
+
+                    foreach ($route['filters'] as $filter) {
+                        if (class_exists($filter)) {
+                            $filter::before();
+                        }
+                    }
+
+                    $response = self::callHandler($route['handler'], $matches);
+
+                    foreach ($route['filters'] as $filter) {
+                        if (class_exists($filter)) {
+                            $filter::after();
+                        }
+                    }
+
+                    foreach (self::$filters as $filter) {
+                        if (class_exists($filter)) {
+                            $filter::after();
+                        }
+                    }
+
+                    return $response;
                 }
             }
         }
-        
-        // 404
-        if (self::$notFound) {
-            return self::callHandler(self::$notFound, []);
+
+        if (!$routeMatched) {
+            if (self::$notFound) {
+                return self::callHandler(self::$notFound, []);
+            }
+            http_response_code(404);
+            echo "404 - Halaman tidak ditemukan";
         }
-        
-        http_response_code(404);
-        echo "404 - Halaman tidak ditemukan";
     }
-    
-    private static function patternToRegex($path) {
+
+    private static function patternToRegex($path)
+    {
         $pattern = preg_replace('/\{[^\}]+\}/', '([^/]+)', $path);
         return "#^$pattern$#";
     }
-    
-    private static function callHandler($handler, $params) {
+
+    private static function callHandler($handler, $params)
+    {
         if (is_string($handler) && strpos($handler, '@') !== false) {
             list($controller, $method) = explode('@', $handler);
-            
-            // Coba dengan namespace App\Controllers
             $controller = "App\\Controllers\\$controller";
             if (class_exists($controller)) {
                 $obj = new $controller();
                 return $obj->$method(...$params);
             }
-            
             throw new \Exception("Controller '$controller' tidak ditemukan");
         }
-        
+
         if (is_callable($handler)) {
             return $handler(...$params);
         }
-        
+
         return $handler;
     }
 }
